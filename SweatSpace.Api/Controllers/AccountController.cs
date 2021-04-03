9b1c95c60d95f98731736c304bcd551c149f1584
chris <christianhunter707@gmail.com>
2021-04-03 19:20:33 +0200

    Add project files.

---
+++ SweatSpace.Api/Controllers/AccountController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SweatSpace.Api.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+
+
+    }
+}
---
+++ SweatSpace.Api/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SweatSpace.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class BaseApiController : ControllerBase
+    {
+    }
+}
