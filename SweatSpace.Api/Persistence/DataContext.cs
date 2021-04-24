427ef0d60fb64edf16d635f3c970cd731f7c4683
chrisk <christian.khalil200@gmail.com>
2021-04-24 19:09:46 +0200

    entity type configuration class

--- SweatSpace.Api/Persistence/DataContext.cs
+++ SweatSpace.Api/Persistence/DataContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SweatSpace.Api.Persistence.Entities;
+using SweatSpace.Api.Persistence.EntityTypeConfigs;
 
 namespace SweatSpace.Persistence.Business
 {
@@ -23,15 +24,8 @@ namespace SweatSpace.Persistence.Business
             builder.Entity<WorkoutExercise>().HasOne<AppUser>()
                 .WithMany().HasForeignKey(x => x.AppUserId).OnDelete(DeleteBehavior.Cascade);
 
-            builder.Entity<AppUser>().HasMany(x => x.LikedWorkouts)
-                .WithMany(x => x.UsersThatLiked);
-
-            builder.Entity<AppUser>().HasMany(x => x.Workouts).WithOne()
-                .HasForeignKey(x => x.AppUserId).OnDelete(DeleteBehavior.Cascade);
-
-            //need to override identity default
-            builder.Entity<AppUser>().HasMany(r => r.Roles).WithOne(u => u.User)
-                .HasForeignKey(u => u.UserId).IsRequired();
+            new AppUserEntityTypeConfig().Configure(builder.Entity<AppUser>());
+            // override identity default
             builder.Entity<AppRole>().HasMany(u => u.Users).WithOne(r => r.Role)
                .HasForeignKey(r => r.RoleId).IsRequired();
         }
---
+++ SweatSpace.Api/Persistence/EntityTypeConfigs/AppUserEntityTypeConfig.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SweatSpace.Api.Persistence.Entities;
+
+namespace SweatSpace.Api.Persistence.EntityTypeConfigs
+{
+    public class AppUserEntityTypeConfig : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder
+                .HasMany(x => x.LikedWorkouts)
+                .WithMany(x => x.UsersThatLiked);
+
+            builder
+                .HasMany(x => x.Workouts)
+                .WithOne().HasForeignKey(x => x.AppUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            //need to override identity default
+            builder
+                .HasMany(r => r.Roles)
+                .WithOne(u => u.User)
+                .HasForeignKey(u => u.UserId)
+                .IsRequired();
+        }
+    }
+}
