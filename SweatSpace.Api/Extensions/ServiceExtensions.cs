5dcf32ee0b53664698fef19cba4f6ae212259146
chrisk <christian.khalil200@gmail.com>
2021-04-28 18:06:58 +0200

    swagger authorization

--- SweatSpace.Api/Extensions/ServiceExtensions.cs
+++ SweatSpace.Api/Extensions/ServiceExtensions.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using Coravel;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
@@ -6,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using SweatSpace.Api.Business.Interfaces;
 using SweatSpace.Api.Business.Invocables;
 using SweatSpace.Api.Business.Services;
@@ -70,6 +74,37 @@ namespace SweatSpace.Api.Extensions
             {
                 opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
             });
+            return services;
+        }
+
+        public static IServiceCollection ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "SweatSpace.Api", Version = "v1" });
+                c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "SweatSpace.Api.xml"));
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT bearer authorization header",
+                    Scheme = "Bearer",
+                    Type = SecuritySchemeType.Http
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                   {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Id = "Bearer",
+                            Type = ReferenceType.SecurityScheme
+                        }
+                    }, new List<string>() }
+                });
+            });
+
             return services;
         }
     }
--- SweatSpace.Api/Startup.cs
+++ SweatSpace.Api/Startup.cs
@@ -1,13 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Coravel;
-using Coravel.Scheduling.Schedule.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using SweatSpace.Api.Business.Invocables;
 using SweatSpace.Api.Extensions;
@@ -31,11 +30,7 @@ namespace SweatSpace.Api
             services.ConfigureIdentityServices(_config);
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SweatSpace.Api", Version = "v1" });
-                c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "SweatSpace.Api.xml"));
-            });
+            services.ConfigureSwagger();          
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
