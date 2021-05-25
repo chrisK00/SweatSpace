5bde38d284c9041d07817fb64357a76c7218d48e
chrisk <christian.khalil200@gmail.com>
2021-05-25 13:35:14 +0200

    Verify setup test

---
+++ SweatSpace.Tests/DependencyInjectionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using SweatSpace.Api;
+using Xunit;
+
+namespace SweatSpace.Tests
+{
+    public class DependencyInjectionTests
+    {
+        [Fact]
+        public void VerifyDISetup()
+        {
+            Program.CreateHostBuilder(Array.Empty<string>())
+                .UseDefaultServiceProvider(config => config.ValidateOnBuild = true).Build();
+        }
+    }
+}
